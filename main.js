8578a5826307bed1464b028eaf793ef09ad4deb8
Zach W <zach39908@gmail.com>
2023-08-24 22:46:59 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('Hello World!');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHNCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vUmVzdGF1cmFudC1MYW5kaW5nLVBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQoJ0hlbGxvIFdvcmxkIScpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
